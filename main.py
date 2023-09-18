01f6d48dc46f4d0fa5db1051268a48868f8e9cc4
Lucas Montebeller <montebellerlucas@gmail.com>
2023-09-17 21:50:36 -0300

    POC inicial para encontrar a relacao candidato x score

--- main.py
+++ main.py
@@ -5,8 +5,65 @@ class Sudoku():
     def __init__(self, grid:np.ndarray):
         self.grid = grid
 
+    @property
+    def grid_size(self):
+        return self.grid.shape
+    
+    @property
+    def grid_reshaped(self):
+        len_square = self.grid_size[0] // 3
+        return np.array([
+            self.grid[i: i + len_square, j: j + len_square] 
+            for i in range(0, self.grid.shape[0], len_square) 
+            for j in range(0, self.grid.shape[0], len_square)
+        ])
+
+    def best_columns(self):
+        index_columns = dict()
+        for j in range(self.grid_size[0]):
+            index_columns[j] = np.count_nonzero(self.grid[:, j])
+
+        return index_columns
+    
+    def best_rows(self):
+        index_rows = dict()
+        for i in range(self.grid_size[0]):
+            index_rows[i] = np.count_nonzero(self.grid[i])
+
+        return index_rows
+    
+    def best_squares(self):
+        interval_index = dict()
+        grid_reshaped = self.grid_reshaped
+        for i in range(grid_reshaped.ndim):
+            for j in range(grid_reshaped.ndim):
+                square = grid_reshaped[i * 3 + j]
+                position_range = (i * 3, i * 3 + 2, j * 3, j * 3 + 2)
+                interval_index[position_range] = np.count_nonzero(square)
+        
+        return interval_index
+    
+    def element_square_score(self, i, j):
+        for position_range, score in self.best_squares().items():
+            i_range, j_range = position_range[:2], position_range[2:]
+            if i_range[0] <= i <= i_range[1] and j_range[0] <= j <= j_range[1]:
+                return score
+
+        Exception("Element not found in square interval")
+            
+
     def choose_element(self):
-        pass
+       rows = self.best_rows()
+       columns = self.best_columns()
+
+       index_zero_rows, index_zero_columns = np.where(self.grid == 0)
+       candidates = dict()
+       for i in index_zero_rows:
+           for j in index_zero_columns:
+            score = rows[i] + columns[j] + self.element_square_score(i, j)
+            candidates[i, j] = score
+            
+       return candidates
 
     def solve(self):
         pass
@@ -14,6 +71,7 @@ class Sudoku():
 def main():
     tabuleiro = pd.read_csv('tabuleiro.csv')
     sudoku = Sudoku(tabuleiro.to_numpy(dtype=np.uint8))
+    sudoku.choose_element()
     print(sudoku.grid)
 
 if __name__ == '__main__':
